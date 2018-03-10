da1a12ea55c032e25740c9f9d29a8b2a49ec71f5
Todd Mantell <tmantell99@yahoo.com>
2018-03-10 08:08:34 -0600

    renamed for clarity and added flow types; added flow preset to the presets array in babelrc

--- src/doublemaker.js
+++ src/doublemaker.js
@@ -1,12 +1,12 @@
-export default values => {
-	const iterator = myGenerator(values);
+export default (stubValues: Array<mixed>): Function => {
+	const iterator = returnMultipleValuesEachWrappedInPromise(stubValues);
 
 	return function () {
 		return iterator.next().value;
 	}
-}
+};
 
-function* myGenerator(arrayOfValues) {
+function* returnMultipleValuesEachWrappedInPromise(arrayOfValues: Array<mixed>): Iterator {
 	for (const item of arrayOfValues) {
 		yield Promise.resolve(item);
 	}
